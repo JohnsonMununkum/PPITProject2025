91a34dfbe53992529c1fa4f5714b443471a22211
Johnson <G00419319@atu.ie>
2025-02-24 14:21:40 +0000

    configured navbar and app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 
 function App() {
   return (
-    
+    //Navigation to other pages in the app
+    <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
   );
 }
 
