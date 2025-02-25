174c668384b70aca09501eccd2dcfc25a5f268c6
Johnson <G00419319@atu.ie>
2025-02-25 16:16:16 +0000

    added styling to homepage

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,12 +1,16 @@
-const Home = ()=>{
+//imported styling from app.css
+import React from "react"; 
+import "../App.css";
+
+const Home =() => {
     //shows whatever is in the return block on the home page
-    return(
-        <div>
+    return (
+        <div className="home">
             <h1>Jay's Cusine</h1>
             <p>Mondern Cantonese Cusine and Irish food, world-class mixology and beautiful scenery</p>
             <p>In the heart of ennis, One of Clare's biggest restaurant.</p>
         </div>
     );
-}
+};
 
 export default Home;
