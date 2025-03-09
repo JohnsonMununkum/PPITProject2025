44f74622beb884c507c1a4b44ec20d0f8de61fb6
Johnson <G00419319@atu.ie>
2025-03-09 22:44:12 +0000

    blank register page created & root added to app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
 import AccountLogin from './components/AccountLogin';
+import Register from './components/Register';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/AccountLogin" element={<AccountLogin/>} />
+          <Route path="/Register" element={<Register/>} />
         </Routes>
     </Router>
   );
---
+++ my-app/src/components/Register.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+//useState to hold the data of these variables
+const Register = () => {
+    
+};
+
+export default Register;
