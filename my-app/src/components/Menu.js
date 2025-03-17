061afc4f5f190fa4776268cc4fbb03b7d54d132f
Johnson <G00419319@atu.ie>
2025-03-17 20:45:00 +0000

    added menu buttons and styling for them

--- my-app/src/components/Menu.js
+++ my-app/src/components/Menu.js
@@ -4,7 +4,11 @@ import "../App.css";
 const Menu = () => {
     return (
             <div className="menu">
-                
+                <div className="menu-options">
+                    <button class="menubtn">Lunch</button>
+                    <button class="menubtn">Evening Menu</button>
+                    <button class="menubtn">Drinks</button>
+                </div>
             </div>
     );
 };
