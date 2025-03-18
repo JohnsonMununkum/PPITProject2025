efb7252f8a3126fc77a5d1986bfbd15a51f57a77
Johnson <G00419319@atu.ie>
2025-03-18 15:54:36 +0000

    some styling for the buttons , when user is on a certain menu they know which one they are on

--- my-app/src/components/Menu.js
+++ my-app/src/components/Menu.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 const Menu = () => {
+    const [activeMenu, setActiveMenu] = useState("lunch"); // Default view
+
     return (
             <div className="menu">
                 <div className="menu-options">
-                    <button class="menubtn">Lunch</button>
-                    <button class="menubtn">Evening Menu</button>
-                    <button class="menubtn">Drinks</button>
+                    <button className={`menubtn ${activeMenu === "lunch" ? "active" : ""}`} onClick={() => setActiveMenu("lunch")}>Lunch</button>
+                    <button  className={`menubtn ${activeMenu === "evening" ? "active" : ""}`}  onClick={() => setActiveMenu("evening")}>Evening Menu</button>
+                    <button  className={`menubtn ${activeMenu === "drinks" ? "active" : ""}`}  onClick={() => setActiveMenu("drinks")}>Drinks</button>
                 </div>
             </div>
     );
