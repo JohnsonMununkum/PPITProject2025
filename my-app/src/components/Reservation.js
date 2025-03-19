ec43e50eb4a16221dda0de0da1819faeaf1563f2
Johnson <G00419319@atu.ie>
2025-03-19 12:16:24 +0000

    variables

--- my-app/src/components/Reservation.js
+++ my-app/src/components/Reservation.js
@@ -1,5 +1,11 @@
+import React, { useState } from 'react';
 
 const Reservation= () =>{
+    //variables
+    const [date, setDate] = useState('');
+    const [time, setTime] = useState('');
+    const [numberOfPeople, setNumberOfPeople] = useState(1);
+    const [message, setMessage] = useState('');
 
 };
 
