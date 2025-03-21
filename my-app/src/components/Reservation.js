194cb4f92d7b07b38e1276eabc8047be96178ff4
Johnson <G00419319@atu.ie>
2025-03-21 13:38:16 +0000

    added more state variables and a check if user is logged in to reservations.js

--- my-app/src/components/Reservation.js
+++ my-app/src/components/Reservation.js
@@ -1,4 +1,7 @@
-import React, { useState } from 'react';
+//importing usenavigate for direction, ude state to store data to specific variables
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Footer from './Footer';
 
 const Reservation= () =>{
     //variables
@@ -6,7 +9,24 @@ const Reservation= () =>{
     const [time, setTime] = useState('');
     const [numberOfPeople, setNumberOfPeople] = useState(1);
     const [message, setMessage] = useState('');
+    const [isError, setIsError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate();
 
+     //time slots
+     const timeSlots = ['6:00 PM', '7:00 PM', '8:00 PM'];
+
+    //check if user is logged in if not direct them to the login page
+    useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+        setMessage('You must be logged in to make a reservation');
+        setIsError(true);
+        setTimeout(() => {
+            navigate('/login');
+        }, 2000);
+    }
+}, [navigate]);
 };
 
 export default Reservation;
