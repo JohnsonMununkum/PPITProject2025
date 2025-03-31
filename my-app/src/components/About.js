19dc5573467b2c77181b5bc3dc8c7251f7804a06
Johnson <G00419319@atu.ie>
2025-03-31 13:25:00 +0000

    added css to the about page

--- my-app/src/components/About.js
+++ my-app/src/components/About.js
@@ -5,19 +5,23 @@ import Footer from "./Footer";
 import Map from "./Map";
 
 const Home =() => {
-    //shows whatever is in the return block on the home page
     return (
         <div className="about">
               <div className="about-container">
-                <p>
-                    Jay's Cuisine is a beloved Irish restaurant nestled in the heart of Ennis. 
-                    We take pride in serving fresh, mouth-watering meals all day long. 
-                    Start your day with our delightful lunch menu, then join us in the evening for our acclaimed dinner selections. 
-                    Whether you're in the mood for a handcrafted cocktail, a fine spirit, or a refreshing non-alcoholic beverage, we've got something for every taste. 
-                    Come and experience our warm, welcoming atmosphere and let us treat you to a truly memorable dining experience!
-                </p>
-
+                <div className="aboutinfo">
+                    <p>
+                        Jay's Cuisine is a beloved Irish restaurant nestled in the heart of Ennis. 
+                        We take pride in serving fresh, mouth-watering meals all day long. 
+                        Start your day with our delightful lunch menu, then join us in the evening for our acclaimed dinner selections. 
+                        Whether you're in the mood for a handcrafted cocktail, a fine spirit, or a refreshing non-alcoholic beverage, we've got something for every taste. 
+                        Come and experience our warm, welcoming atmosphere and let us treat you to a truly memorable dining experience!
+                    </p>
+                </div>
+    
+            <div className="map">
                 <Map/>
+            </div>
+
             </div>
 
             <Footer/>
